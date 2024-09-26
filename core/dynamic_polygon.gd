335a082dae557805bfdd26b1ce8ef85661e2ba92
ChaoticByte <contact@chaoticbyte.net>
2024-09-26 20:10:56 +0200

    Added rain with terrain collision

--- core/dynamic_polygon.gd
+++ core/dynamic_polygon.gd
@@ -13,8 +13,13 @@ extends CollisionPolygon2D
 		update_polygon()
 
 func update_polygon():
+	# normal polygon
 	$Polygon2D.polygon = polygon
 	$Polygon2D.color = color
+	# light occluder
+	var lo_polygon = OccluderPolygon2D.new()
+	lo_polygon.polygon = polygon
+	$LightOccluder2D.occluder = lo_polygon
 
 func _ready() -> void:
 	update_polygon()
